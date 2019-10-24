936110210b6a0a1d95c6702f48f7cc81a7a67752
Michael Kohler <me@michaelkohler.info>
2019-10-24 20:57:46 +0200

    Update fields and form

--- public/client.js
+++ public/client.js
@@ -1,28 +1,5 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const fenixVersionInput = document.querySelector('#fenix');
-  fenixVersionInput.value = navigator.userAgent;
-
-  const androidVersionInput = document.querySelector('#android');
-  const androidVersion = getAndroidVersion();
-  if (androidVersion) {
-    androidVersionInput.value = androidVersion;
-  }
-
-  const deviceInput = document.querySelector('#device');
-  deviceInput.addEventListener('change', (evt) => {
-    const value = evt.target.value;
-    localStorage.setItem('FENIX_BUG_DEVICE', value);
-  });
-  const savedDevice = localStorage.getItem('FENIX_BUG_DEVICE');
-  if (savedDevice) {
-    deviceInput.value = savedDevice;
-  }
+  console.log('TODO.. saving and restoring devices..');
 });
-
-function getAndroidVersion(ua = navigator.userAgent) {
-  const userAgent = ua.toLowerCase();
-  const match = userAgent.match(/Android\s(([0-9]+\.*)*)/i);
-  return match ? match[1] : undefined;
-};
