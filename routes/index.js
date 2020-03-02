325df391bf80363ee7d09fb5805876a7d3a3094f
Michael Kohler <me@michaelkohler.info>
2020-03-02 21:46:22 +0100

    Prepare for private GitHub repo

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,9 @@ const { OWNER, REPO } = process.env;
 
 router.get('/', async (req, res) => {
   debug('INCOMING_REQUEST_INDEX');
-  const issue = `https://github.com/${OWNER}/${REPO}/issues/${req.query.issue}`
   res.render('index', {
     success: /true/.test(req.query.success),
     submitted: /true/.test(req.query.submitted),
-    issue,
   });
 });
 
@@ -26,7 +24,7 @@ router.post('/create', async (req, res) => {
     const issueInfo = await githubBackend.createIssue(fields);
     const issueNumber = issueInfo && issueInfo.data && issueInfo.data.number;
     debug('ISSUE_CREATED_REDIRECTING_BACK', issueNumber);
-    res.redirect(`/?success=true&submitted=true&issue=${issueNumber}`);
+    res.redirect(`/?success=true&submitted=true`);
   } catch(error) {
     debug('OH_NO_COULD_NOT_CREATE', error.message);
     res.redirect('/?success=false&submitted=true');
